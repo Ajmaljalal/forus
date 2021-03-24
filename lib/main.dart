98f69505a6ddf541d36d7a70ea6e7d85ebc356a2
Ajmal Jalal <ajmaljalal58@gmail.com>
2021-03-23 21:11:27 -0700

    web innver navigation finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+// import 'package:device_preview/device_preview.dart';
 // import 'package:url_strategy/url_strategy.dart';
 import 'package:flutter/services.dart';
 import 'package:forus/controllers/home_controllers/home_controllers.dart';
@@ -13,15 +14,22 @@ void main() {
     ),
   );
   // setPathUrlStrategy();
-  runApp(Application());
+  runApp(
+    // DevicePreview(
+    Application(),
+    // builder: (context) => Application(),
+    // ),
+  );
 }
 
 class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      // locale: DevicePreview.locale(context), // Add the locale here
+      // builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
-      title: 'Nagashi',
+      title: 'Batwa',
       initialRoute: '/',
       defaultTransition: Transition.noTransition,
       theme: ThemeData(fontFamily: 'Helvetica, Arial, sans-serif'),
